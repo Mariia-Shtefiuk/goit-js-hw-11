b0f4ddc43bcdd33836ee19597f5b4ad915a5d394
Mariia Shtefiuk <masha.shtf@gmail.com>
2025-06-21 17:54:22 +0300

    pixabay-api, main js modify

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,5 +1,17 @@
 import axios from 'axios';
 
-function getImagesByQuery(query) {
-  return axios.get();
+const API_KEY = '50948492-7a2c95c1e3ac536ddbec900a1';
+
+export function getImagesByQuery(query) {
+  return axios
+    .get('https://pixabay.com/api/', {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    })
+    .then(response => response.data);
 }
--- src/main.js
+++ src/main.js
@@ -2,14 +2,20 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import { getImagesByQuery } from './js/pixabay-api';
 
 const form = document.querySelector('.form');
 form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
-  const input = form.querySelector('input[name="search-text"]').value;
-  if (!input.length == 0) {
-    console.log('поле не порожнє');
+  const input = form.querySelector('input[name="search-text"]').value.trim();
+  if (input === '') {
+    iziToast.error({
+      title: 'Error',
+      message: 'Поле не повинно бути порожнім',
+      position: 'topRight',
+    });
+    return;
   }
 }
