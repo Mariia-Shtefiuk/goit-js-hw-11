a12c5c711452e663b8a37357551f3fb600a1bb50
Mariia Shtefiuk <masha.shtf@gmail.com>
2025-06-22 00:59:22 +0300

    js files update

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,7 +1,62 @@
-const render = new SimpleLightbox();
-function createGallery(images) {}
-function clearGallery() {}
-function showLoader() {}
-function hideLoader() {}
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const gallery = document.querySelector('.gallery');
+const lightbox = new SimpleLightbox('.gallery a');
+const loader = document.querySelector('.loader');
+
+export const createGallery = images => {
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return ` 
+  <li class="gallery-item">
+        <a href="${largeImageURL}">
+        <img src="${webformatURL}" alt="${tags}" width="300">
+        </a>
+        <div class="image-stats">
+            <div class="image-stat">
+                <span class="label">Likes</span>
+                <span class="value">${likes}</span>
+            </div>
+            <div class="image-stat">
+                <span class="label">Views</span>
+                <span class="value">${views}</span>
+            </div>
+            <div class="image-stat">
+                <span class="label">Comments</span>
+                <span class="value">${comments}</span>
+            </div>
+            <div class="image-stat">
+                <span class="label">Downloads</span>
+                <span class="value">${downloads}</span>
+            </div>
+        </div>
+    </li>
+  `;
+      }
+    )
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+};
+
+export function clearGallery() {
+  gallery.innerHTML = '';
+}
+
+export function showLoader() {
+  loader.classList.remove('hidden');
+}
+
+export function hideLoader() {
+  loader.classList.add('hidden');
+}
--- src/main.js
+++ src/main.js
@@ -1,24 +1,54 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
 import { getImagesByQuery } from './js/pixabay-api.js';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions.js';
 
 const form = document.querySelector('.form');
-form.addEventListener('submit', handleSubmit);
 
+form.addEventListener('submit', handleSubmit);
 function handleSubmit(event) {
   event.preventDefault();
-  const input = form.querySelector('input[name="search-text"]').value.trim();
-  if (input === '') {
-    iziToast.error({
-      title: 'Error',
-      message: 'Поле не повинно бути порожнім',
+
+  const q = form.querySelector('input[name="search-text"]').value.trim();
+  if (q === '') {
+    iziToast.warning({
+      title: 'Увага',
+      message: 'Поле пошуку не може бути поржнім!',
       position: 'topRight',
     });
     return;
   }
-  getImagesByQuery(input).then(data => {
-    console.log(data);
-  });
+
+  clearGallery();
+  showLoader();
+
+  getImagesByQuery(q)
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.error({
+          title: 'Oops',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+        return;
+      }
+      createGallery(data.hits);
+    })
+    .catch(error => {
+      iziToast.error({
+        title: 'Error',
+        message: 'Something went wrong. Please try again later',
+        position: 'topRight',
+      });
+    })
+    .finally(() => {
+      hideLoader();
+    });
 }
